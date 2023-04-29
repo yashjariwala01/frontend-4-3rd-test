33febe4ed0d2920524a521a2dd77160a0ecf1115
yashjariwala01 <jariwala.yash02@gmail.com>
2023-04-29 21:58:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
+import ProductsContainer from './components/Products-container';
+import {useSelector, useDispatch} from 'react-redux';
+import {fetchData} from './actions';
+import Cart from './components/CartPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
+
+  const loading  = useSelector((state) =>state.data.loading)
+     const data  = useSelector((state) =>state.data.data)
+     const error = useSelector((state) =>state.data.error)
+    
+      const dispatch = useDispatch()
+    
+     useEffect(() =>{
+          dispatch(fetchData())
+     }, [dispatch]
+     )
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <h1>Error: {error}</h1>
+    }
+  
+    
+
+ 
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<ProductsContainer prodData={data} />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+            <div className='brand-name'>
+                <NavLink exact activeClassName="active_class" to="/"><b>Delta Shopping app</b></NavLink>
+            </div>
+            <div className='links-div'>
+            <NavLink exact activeClassName="active_class" to="/">Home Page</NavLink>
+            <NavLink exact activeClassName="active_class" to="/cart">Shopping Cart</NavLink>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
